1d3f8cfbe0894a21bf167e44707a5dcf93c7a750
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-11-07 01:47:50 +0300

    DF_PHPUNIT_STORE

--- app/code/local/Df/t.php
+++ app/code/local/Df/t.php
@@ -8,7 +8,9 @@ require_once "{$base}/app/Mage.php";
 Mage::setIsDeveloperMode(true);
 Mage::app('default');
 // 2016-11-07
-// Пример настройки: SetEnv DF_PHPUNIT_STORE "en"
+// Если Ваша система содержит несколько витрин, и ни одна из них не имеет «default»,
+// то надо указать витрину, которая будет использоваться PHPUnit, в настройках среды разработки.
+// В IntelliJ IDEA это делается в графе «Environment variables» на экране настроек PHPUnit.
 Mage::dispatchEvent(@$_SERVER['DF_PHPUNIT_STORE'] ?: 'default');
 /** http://stackoverflow.com/a/4059399 */
 ob_start();
